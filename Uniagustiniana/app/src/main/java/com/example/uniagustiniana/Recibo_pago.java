57357693e69fb05b71d5f2426c52646b26ba1b2e
Luisvalbuenag <luis.valbuenag@uniagustiniana.edu.co>
2022-10-13 20:08:37 -0500

    parcial

---
+++ Uniagustiniana/app/src/main/java/com/example/uniagustiniana/Recibo_pago.java
@@ -0,0 +1,14 @@
+package com.example.uniagustiniana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recibo_pago extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recibo_pago);
+    }
+}
---
+++ Uniagustiniana/app/src/main/java/com/example/uniagustiniana/Universidad.java
@@ -0,0 +1,2 @@
+package com.example.uniagustiniana;public class Universidad {
+}
