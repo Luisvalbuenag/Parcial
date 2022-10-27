97fc8d74546185b9d3688d36556a438d99598705
Luisvalbuenag <luis.valbuenag@uniagustiniana.edu.co>
2022-10-26 23:28:41 -0500

    Aplicacion terminada

--- Uniagustiniana/app/src/main/java/com/example/uniagustiniana/Recibo_pago.java
+++ Uniagustiniana/app/src/main/java/com/example/uniagustiniana/Recibo_pago.java
@@ -3,12 +3,65 @@ package com.example.uniagustiniana;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Recibo_pago extends AppCompatActivity {
+
+    public TextView nombre;
+    public TextView apellido;
+    public TextView telefono;
+    public TextView cedula;
+    public TextView direccion;
+    public TextView creditos;
+    public TextView totalcreditos;
+    public TextView totalcreditosiva;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recibo_pago);
+
+
+        nombre = findViewById(R.id.Nombre);
+        apellido = findViewById(R.id.Apellido);
+        cedula = findViewById(R.id.Cedula);
+        telefono = findViewById(R.id.Creditos);
+        direccion = findViewById(R.id.Direccion);
+        creditos = findViewById(R.id.Telefono);
+        totalcreditos = findViewById(R.id.TotalCreditos);
+        totalcreditosiva = findViewById(R.id.TotalCreditosIva);
+
+        Bundle bundle1 = getIntent().getExtras();
+        Universidad dato = null;
+
+
+            dato = (Universidad) bundle1.getSerializable("univer");
+            nombre.setText(dato.getNombre());
+            apellido.setText(dato.getApellido());
+            cedula.setText(String.valueOf(dato.getCedula()));
+            telefono.setText(dato.getTelefono());
+            direccion.setText(dato.getDireccion());
+            creditos.setText(String.valueOf(dato.getCredito()));
+
+           double creditos1 = Double.parseDouble(dato.getCredito());
+
+
+          totalcreditos.setText(String.valueOf(dato.operacion(345000,Double.parseDouble(dato.getTelefono()))));
+
+          double creditostotal = dato.operacion(345000,Double.parseDouble(dato.getTelefono()));
+
+          double Totalfinal =  (creditostotal * 0.19) + creditostotal;
+
+           totalcreditosiva.setText(String.valueOf(Totalfinal));
+
+
+
     }
-}
+
+
+
+   }
+
+
+
--- Uniagustiniana/app/src/main/java/com/example/uniagustiniana/Universidad.java
+++ Uniagustiniana/app/src/main/java/com/example/uniagustiniana/Universidad.java
@@ -1,2 +1,92 @@
-package com.example.uniagustiniana;public class Universidad {
+package com.example.uniagustiniana;
+
+import java.io.Serializable;
+
+public class Universidad implements Serializable {
+
+    public String nombre;
+    public String apellido;
+    public String direccion;
+    public String cedula;
+    public String telefono;
+    public String credito;
+    int resultado;
+
+    //,int cedula,int credito,int telefono
+
+    public Universidad(String nombre, String apellido, String direccion, String cedula, String telefono, String credito) {
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.cedula = cedula;
+        this.credito = credito;
+        this.telefono = telefono;
+    }
+
+    public Universidad(String nombre, String apellido, String direccion, int cedula, int credito, int telefono) {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCredito() {
+        return credito;
+    }
+
+    public void setCredito(String credito) {
+        this.credito = credito;
+    }
+
+    public double operacion(double costocreditouni, double numerocreditos) {
+
+        if (numerocreditos == 1) {
+
+            return costocreditouni;
+        }else {
+
+            return costocreditouni + operacion(costocreditouni, numerocreditos - 1);
+        }
+
+
+    }
+
 }
